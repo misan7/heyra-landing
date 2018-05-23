0a0bc78bdb2717ad808b5967d6c084cd7fb3fa5d
fdelpozo <felipedelpozo@dtoo.es>
2018-05-23 10:42:02 +0200

    Form Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,154 +1,119 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import NetlifyForm from 'react-netlify-form';
 
 import Social from './Social';
 
-class Contact extends React.Component {
-  state = {};
-
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = (e) => {
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({ 'form-name': 'contact', ...this.state })
-    })
-      .then(() => navigateTo('/thanks/'))
-      .catch((error) => alert(error));
-
-    e.preventDefault();
-  };
-
-  render() {
-    return (
-      <section id="contact" className="s-contact">
-        <div className="overlay" />
-        <div className="contact__line" />
-        <div className="row section-header" data-aos="fade-up">
-          <div className="col-full">
-            <h3 className="subhead">Contacto</h3>
-            <h1 className="display-2 display-2--light">
-              Si necesitas más información
-            </h1>
-          </div>
-        </div>
-        <div className="row contact-content" data-aos="fade-up">
-          <div className="contact-primary">
-            <h3 className="h6">Envía un mensaje</h3>
-            <form
-              name="contactForm"
-              id="contactForm"
-              method="post"
-              action="true"
-              noValidate="novalidate"
-            >
-              <fieldset>
-                <div className="form-field">
-                  <input
-                    name="contactName"
-                    type="text"
-                    id="contactName"
-                    placeholder="Tú nombre"
-                    defaultValue
-                    required
-                    aria-required="true"
-                    className="full-width"
-                  />
-                </div>
-                <div className="form-field">
-                  <input
-                    name="contactEmail"
-                    type="email"
-                    id="contactEmail"
-                    placeholder="Tu E-Mail"
-                    defaultValue
-                    required
-                    aria-required="true"
-                    className="full-width"
-                  />
-                </div>
-                <div className="form-field">
-                  <input
-                    name="contactSubject"
-                    type="text"
-                    id="contactSubject"
-                    placeholder="Asunto"
-                    defaultValue
-                    className="full-width"
-                  />
-                </div>
-                <div className="form-field">
-                  <textarea
-                    name="contactMessage"
-                    id="contactMessage"
-                    placeholder="El mensaje"
-                    rows={10}
-                    cols={50}
-                    required
-                    aria-required="true"
-                    className="full-width"
-                    defaultValue={''}
-                  />
-                </div>
-                <div className="form-field">
-                  <button className="full-width btn--primary">Continuar</button>
-                  <div className="submit-loader">
-                    <div className="text-loader">Sending...</div>
-                    <div className="s-loader">
-                      <div className="bounce1" />
-                      <div className="bounce2" />
-                      <div className="bounce3" />
+const Contact = () => (
+  <section id="contact" className="s-contact">
+    <div className="overlay" />
+    <div className="contact__line" />
+    <div className="row section-header" data-aos="fade-up">
+      <div className="col-full">
+        <h3 className="subhead">Contacto</h3>
+        <h1 className="display-2 display-2--light">
+          Si necesitas más información
+        </h1>
+      </div>
+    </div>
+    <div className="row contact-content" data-aos="fade-up">
+      <div className="contact-primary">
+        <NetlifyForm>
+          {(formState) => (
+            <div>
+              {formState.loading && 'Loading...'}
+              {!formState.success && (
+                <div>
+                  <h3 className="h6">Envía un mensaje</h3>
+                  <fieldset>
+                    <div className="form-field">
+                      <input
+                        name="contactName"
+                        type="text"
+                        id="contactName"
+                        placeholder="Tú nombre"
+                        aria-required="true"
+                        className="full-width"
+                        required
+                      />
                     </div>
-                  </div>
+                    <div className="form-field">
+                      <input
+                        name="contactEmail"
+                        type="email"
+                        id="contactEmail"
+                        placeholder="Tu E-Mail"
+                        aria-required="true"
+                        className="full-width"
+                        required
+                      />
+                    </div>
+                    <div className="form-field">
+                      <input
+                        name="contactSubject"
+                        type="text"
+                        id="contactSubject"
+                        placeholder="Asunto"
+                        className="full-width"
+                        required
+                      />
+                    </div>
+                    <div className="form-field">
+                      <textarea
+                        name="contactMessage"
+                        id="contactMessage"
+                        placeholder="El mensaje"
+                        rows={10}
+                        cols={50}
+                        aria-required="true"
+                        className="full-width"
+                        required
+                      />
+                    </div>
+                    <div className="form-field">
+                      <button className="full-width btn--primary">
+                        Continuar
+                      </button>
+                    </div>
+                  </fieldset>
                 </div>
-              </fieldset>
-            </form>
-            <div className="message-warning">
-              Something went wrong. Please try again.
+              )}
+              {formState.error && (
+                <div className="message-warning">
+                  Something went wrong. Please try again.
+                </div>
+              )}
+              {formState.success && (
+                <div className="message-success">
+                  Your message was sent, thank you!
+                </div>
+              )}
             </div>
-            <div className="message-success">
-              Your message was sent, thank you!
-              <br />
-            </div>
+          )}
+        </NetlifyForm>
+      </div>
+      <div className="contact-secondary">
+        <div className="contact-info">
+          <h3 className="h6 hide-on-fullwidth">Información de contacto</h3>
+          <div className="cinfo">
+            <h5>Por e-mail</h5>
+            <p>
+              <a href="mailto:comercial@botspecialist.es">
+                comercial@botspecialist.es
+              </a>
+            </p>
           </div>
-          <div className="contact-secondary">
-            <div className="contact-info">
-              <h3 className="h6 hide-on-fullwidth">Contact Info</h3>
-              <div className="cinfo">
-                <h5>Where to Find Us</h5>
-                <p>
-                  1600 Amphitheatre Parkway
-                  <br /> Mountain View, CA
-                  <br /> 94043 US
-                </p>
-              </div>
-              <div className="cinfo">
-                <h5>Email Us At</h5>
-                <p>
-                  contact@glintsite.com
-                  <br /> info@glintsite.com
-                </p>
-              </div>
-              <div className="cinfo">
-                <h5>Call Us At</h5>
-                <p>
-                  Phone: (+63) 555 1212
-                  <br /> Mobile: (+63) 555 0100
-                  <br /> Fax: (+63) 555 0101
-                </p>
-              </div>
-              <Social className="contact-social" />
-            </div>
+          <div className="cinfo">
+            <h5>Por teléfono</h5>
+            <p>
+              <a href="tel:675985062">(+34) 675 985 062</a>
+            </p>
           </div>
+          <Social className="contact-social" />
         </div>
-      </section>
-    );
-  }
-}
-
-Contact.propTypes = {};
+      </div>
+    </div>
+  </section>
+);
 
 export default Contact;
