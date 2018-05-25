6c8a0ab3413467693ff37f19314a3171d7b3a187
fdelpozo <felipedelpozo@dtoo.es>
2018-05-25 12:15:57 +0200

    Add mobile background

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import YouTube from 'react-youtube';
-import Background from '../img/hero-bg.jpg';
+import Image from 'gatsby-image';
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from 'react-device-detect';
 import Social from './Social';
 
 class Home extends Component {
@@ -16,37 +22,32 @@ class Home extends Component {
   }
 
   render() {
-    const { title, slogan, backgroundVideo } = this.props;
+    const { title, slogan, background, backgroundVideo } = this.props;
 
     return (
-      <section
-        id="home"
-        className="s-home target-section"
-        data-parallax="scroll"
-        data-natural-width={3000}
-        data-natural-height={2000}
-        data-position-y="center"
-      >
-        <div className="video-background">
-          <div className="video-foreground">
-            <YouTube
-              videoId={backgroundVideo}
-              opts={{
-                playerVars: {
-                  // https://developers.google.com/youtube/player_parameters
-                  autoplay: 1,
-                  controls: 0,
-                  loop: 1,
-                  rel: 0,
-                  showinfo: 0
-                }
-              }}
-              className="video-iframe"
-              onReady={this._onReady}
-              onEnd={this._onEnd}
-            />
+      <section id="home" className="s-home target-section">
+        <BrowserView device={isBrowser}>
+          <div className="video-background">
+            <div className="video-foreground">
+              <YouTube
+                videoId={backgroundVideo}
+                opts={{
+                  playerVars: {
+                    // https://developers.google.com/youtube/player_parameters
+                    autoplay: 1,
+                    controls: 0,
+                    loop: 1,
+                    rel: 0,
+                    showinfo: 0
+                  }
+                }}
+                className="video-iframe"
+                onReady={this._onReady}
+                onEnd={this._onEnd}
+              />
+            </div>
           </div>
-        </div>
+        </BrowserView>
 
         <div className="overlay" />
         <div className="shadow-overlay" />
@@ -85,7 +86,8 @@ class Home extends Component {
 Home.propTypes = {
   title: PropTypes.string,
   slogan: PropTypes.string,
-  backgroundVideo: PropTypes.string
+  backgroundVideo: PropTypes.string,
+  background: PropTypes.object
 };
 
 export default Home;
