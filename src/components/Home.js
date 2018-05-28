fae5bda41d3b4e9910a12a366c855e99db850a84
fdelpozo <felipedelpozo@dtoo.es>
2018-05-28 14:42:01 +0200

    Change bot

--- src/components/Home.js
+++ src/components/Home.js
@@ -49,13 +49,11 @@ class Home extends Component {
           </div>
         </BrowserView>
         <MobileView device={isMobile} />
-
         <div className="overlay" />
         <div className="shadow-overlay" />
         <div className="home-content">
           <div className="row home-content__main">
-            <h3>{title}</h3>
-            <h1>{slogan}</h1>
+            <h3> {title} </h3> <h1> {slogan} </h1>
             <div className="home-content__buttons">
               <a
                 href="https://api.whatsapp.com/send?phone=34699914244&text=Hola"
@@ -73,7 +71,7 @@ class Home extends Component {
           </div>
           <div className="home-content__scroll">
             <a href="#about" className="scroll-link smoothscroll">
-              <span>Aún hay más</span>
+              <span> Aún hay más </span>
             </a>
           </div>
           <div className="home-content__line" />
