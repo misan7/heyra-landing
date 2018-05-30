91e56a1f6621309e0c6f094942743e3492b2e570
fdelpozo <felipedelpozo@dtoo.es>
2018-05-30 18:21:54 +0200

    Bugfix chatbot

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@
 // https://github.com/dialogflow/dialogflow-fulfillment-nodejs/blob/master/docs/WebhookClient.md#WebhookClient+session
 const functions = require('firebase-functions');
 const { WebhookClient } = require('dialogflow-fulfillment');
-const { Notification, User } = require('./helpers');
+const { Notification, User, sendOffer } = require('./etc/helpers');
 
 // const { Card, Suggestion } = require('dialogflow-fulfillment');
 
@@ -16,21 +16,30 @@ exports.fulfillment = functions.https.onRequest((request, response) => {
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-  const sendOffer = () => {
-    // Register user
-    User(agent.session || request.body.sessionId).then((user) => {
-      user
-        .addContexts(agent.intent, agent.contexts)
-        .then(() => console.log('Contexts added!'));
-    });
-
-    agent.add('Esta es tu oferta de Securitas');
-    agent.setFollowupEvent({ name: 'OFFER_SECURITAS' });
-  };
-
   let intentMap = new Map();
-  if (agent.action === 'offer.send') {
-    intentMap.set(null, sendOffer);
+  let handleName = null;
+
+  switch (agent.action) {
+    case 'offer.send':
+      // Register user
+      User(agent.session || agent.originalRequest.body.sessionId).then(
+        (user) => {
+          user
+            .addContexts(agent.intent, agent.contexts)
+            .then(() => console.log('Contexts added!'));
+        }
+      );
+
+      handleName = sendOffer;
+      break;
+    case 'notification.send':
+      handleName = sendNotification;
+      break;
   }
+
+  if (handleName) {
+    intentMap.set(null, handleName);
+  }
+
   return agent.handleRequest(intentMap);
 });
