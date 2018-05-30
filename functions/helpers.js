0a21cb9f63c4a0e7f2a9cb01813979ebd714412c
fdelpozo <felipedelpozo@dtoo.es>
2018-05-30 11:56:06 +0200

    First funtional version

--- functions/helpers.js
+++ functions/helpers.js
@@ -1,6 +1,16 @@
 'use strict';
 
+// https://cloud.google.com/nodejs/docs/reference/firestore/0.13.x/?authuser=0
+
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const defaults = require('lodash/defaults');
+const map = require('lodash/map');
+const omitBy = require('lodash/omitBy');
+
 const nodemailer = require('nodemailer');
+
+admin.initializeApp(functions.config().firebase);
 
 exports.Notification = ({ user, pass, notification }) => {
   const transport = nodemailer.createTransport({
@@ -19,3 +29,52 @@ exports.Notification = ({ user, pass, notification }) => {
     }
   };
 };
+
+exports.User = (userId) =>
+  new Promise((resolve, reject) =>
+    admin
+      .firestore()
+      .collection('users')
+      .where('userId', '==', userId)
+      .limit(1)
+      .get()
+      .then((snapshot) => {
+        const now = () => new Date().toISOString();
+        const isOriginal = /\.original/;
+        const extractParameters = (contexts) => {
+          let values = {};
+          map(contexts, 'parameters').forEach((obj) =>
+            defaults(values, omitBy(obj, (val, key) => isOriginal.test(key)))
+          );
+          return values;
+        };
+
+        const methods = (ref) => ({
+          addContexts: (intentName, contexts) =>
+            new Promise((resolve, reject) => {
+              console.log(
+                'Parameters: ' + JSON.stringify(extractParameters(contexts))
+              );
+
+              ref
+                .collection('contexts')
+                .add({ contexts, intentName, created_at: now() })
+                .then(() =>
+                  ref.update(extractParameters(contexts)).then(resolve)
+                );
+            })
+        });
+
+        const doc = snapshot.docs[0];
+
+        if (doc) {
+          return resolve(methods(doc.ref));
+        }
+
+        admin
+          .firestore()
+          .collection('users')
+          .add({ userId, created_at: now() })
+          .then((ref) => resolve(methods(ref)));
+      })
+  );
--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,11 @@
 
 // https://github.com/dialogflow/dialogflow-fulfillment-nodejs/blob/master/docs/WebhookClient.md#WebhookClient+session
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 const { WebhookClient } = require('dialogflow-fulfillment');
-const { Notification } = require('./helpers');
+const { Notification, User } = require('./helpers');
 
 // const { Card, Suggestion } = require('dialogflow-fulfillment');
 
-admin.initializeApp(functions.config().firebase);
 process.env.DEBUG = 'dialogflow:debug';
 
 const notification = Notification(functions.config().gmail);
@@ -18,38 +16,21 @@ exports.fulfillment = functions.https.onRequest((request, response) => {
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-  if (agent.action === 'user.upgrade') {
-    const userId = agent.session || request.body.sessionId;
+  const sendOffer = () => {
+    // Register user
+    User(agent.session || request.body.sessionId).then((user) => {
+      user
+        .addContexts(agent.intent, agent.contexts)
+        .then(() => console.log('Contexts added!'));
+    });
 
-    console.log(agent.contexts);
+    agent.add('Esta es tu oferta de Securitas');
+    agent.setFollowupEvent({ name: 'OFFER_SECURITAS' });
+  };
 
-    admin
-      .firestore()
-      .collection('users')
-      .where('userId', '==', userId)
-      .limit(1)
-      .get()
-      .then((snapshot) => {
-        let user = snapshot.docs[0];
-        if (!user) {
-          // Add the user to DB
-          admin
-            .firestore()
-            .collection('users')
-            .add({
-              userId: userId
-            })
-            .then((ref) => {
-              notification
-                .createUser(user)
-                .then(() =>
-                  console.log(`Added new user with sessionId ${userId}`)
-                );
-            });
-        } else {
-          // User in DB
-          console.log(`User already exists with sessionId ${userId}`);
-        }
-      });
+  let intentMap = new Map();
+  if (agent.action === 'offer.send') {
+    intentMap.set(null, sendOffer);
   }
+  return agent.handleRequest(intentMap);
 });
