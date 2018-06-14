53e07618e2ff410c03799031344e862120a80e51
fdelpozo <info@botspecialist.es>
2018-06-14 16:42:45 +0200

    Run Chatbot is visible

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import VisibilitySensor from 'react-visibility-sensor';
 
 const hour = (date) => {
   return `${date.getHours() < 10 ? '0' + date.getHours() : date.getHours()}:${
@@ -46,55 +47,42 @@ export default class Chatbot extends Component {
         'Con pocas preguntas, te daré la solución más adecuada a tus necesidades.',
         { delay: 500, timestamp: now }
       )
-      .fbMessenger(
-        'message',
-        'right',
-        'Una casa',
-        { delay: 2000, timestamp: now }
-      )
-      .fbMessenger(
-        'message',
-        'left',
-        '¿Es la vivienda donde resides?',
-        { delay: 500, timestamp: now }
-      )
-      .fbMessenger(
-        'message',
-        'right',
-        'Si',
-        { delay: 2000, timestamp: now }
-      )
+      .fbMessenger('message', 'right', 'Una casa', {
+        delay: 2000,
+        timestamp: now
+      })
+      .fbMessenger('message', 'left', '¿Es la vivienda donde resides?', {
+        delay: 500,
+        timestamp: now
+      })
+      .fbMessenger('message', 'right', 'Si', { delay: 2000, timestamp: now })
       .fbMessenger('typingIndicator', { delay: 1000, timestamp: now })
-      .fbMessenger(
-        'message',
-        'left',
-        '¿Cuántos metros cuadrados tiene?',
-        { delay: 500, timestamp: now }
-      )
-      .fbMessenger(
-        'message',
-        'right',
-        '90',
-        { delay: 2000, timestamp: now }
-      )
+      .fbMessenger('message', 'left', '¿Cuántos metros cuadrados tiene?', {
+        delay: 500,
+        timestamp: now
+      })
+      .fbMessenger('message', 'right', '90', { delay: 2000, timestamp: now })
       .fbMessenger('typingIndicator', { delay: 1000, timestamp: now })
-      .fbMessenger(
-        'message',
-        'left',
-        '¿Cuál es el tu código postal?',
-        { delay: 500, timestamp: now }
-      )
-      .fbMessenger(
-        'message',
-        'right',
-        '08520',
-        { delay: 2000, timestamp: now }
-      )
-      .fbMessenger('run');
+      .fbMessenger('message', 'left', '¿Cuál es el tu código postal?', {
+        delay: 500,
+        timestamp: now
+      })
+      .fbMessenger('message', 'right', '08520', {
+        delay: 2000,
+        timestamp: now
+      });
   }
+
+  onChange(isVisible) {
+    if (isVisible && this.$el) {
+      this.$el.fbMessenger('run');
+    }
+  }
+
   render() {
     return (
       <div id="chatbot-content">
+        <VisibilitySensor onChange={(isVisible) => this.onChange(isVisible)} />
         <div id="chatbot">
           <div className="finger" />
           <div className="screen-content" ref={(el) => (this.el = el)} />
