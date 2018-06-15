44785c1e073b5cc3a8d2b70079661b9cdc3f187a
fdelpozo <info@botspecialist.es>
2018-06-15 12:19:05 +0200

    Link style

--- src/templates/link.js
+++ src/templates/link.js
@@ -4,9 +4,9 @@ import Helmet from 'react-helmet';
 
 import { btn } from '../components/Social';
 
-export const LinkTemplate = ({ title, link }) => {
+export const LinkTemplate = ({ title, name, link }) => {
   return (
-    <section className="s-link">
+    <section className="s-page">
       <Helmet
         bodyAttributes={{
           onload: `window.location='${link}'`
@@ -16,9 +16,18 @@ export const LinkTemplate = ({ title, link }) => {
       </Helmet>
       <div className="row section-header has-bottom-sep">
         <div className="col-full">
-          <h3 className="subhead">{link}</h3>
+          <h3 className="subhead">chatbot</h3>
           <h1 className="display-2">{title}</h1>
+          <p>
+            Si has llegado hasta aquí, es por que quieres chatear mediante{' '}
+            {name}
+          </p>
         </div>
+      </div>
+      <div className="row section-header">
+        <a href={link} className="contact btn">
+          Empezar a chatear
+        </a>
       </div>
     </section>
   );
@@ -26,6 +35,7 @@ export const LinkTemplate = ({ title, link }) => {
 
 LinkTemplate.propTypes = {
   title: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired
 };
 
@@ -33,7 +43,13 @@ const Link = ({ data }) => {
   const { markdownRemark: post } = data;
   const link = btn(post.frontmatter.name).link;
 
-  return <LinkTemplate title={post.frontmatter.title} link={link} />;
+  return (
+    <LinkTemplate
+      title={post.frontmatter.title}
+      link={link}
+      name={post.frontmatter.name}
+    />
+  );
 };
 
 Link.propTypes = {
