9fdf3986f01cd6ad3224525c2ff11ba8dde90dd3
fdelpozo <info@botspecialist.es>
2018-06-19 12:00:46 +0200

    Messenger

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -7,6 +7,10 @@ export default class ChatBot extends Component {
       <MessengerCustomerChat
         pageId="2080068382275303"
         appId="317328275466661"
+        themeColor="#13cf13"
+        loggedInGreeting="¡Hola! Soy Alarmbots, un bot que compara gratis alarmas para hogar y negocio."
+        loggedOutGreeting="¡Hola! Soy Alarmbots, un bot que compara gratis alarmas para hogar y negocio."
+        language="es_ES"
       />
     );
   }
